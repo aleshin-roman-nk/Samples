d49e0aa4bd8cf540e2f1972e17e6dae53722ed2a
Roman Aleshin <aleshin.roman.nk@gmail.com>
2023-06-08 07:56:19 +0700

    08-06-2023

--- angular/lesson-comun-v2/src/app/model.ts
+++ angular/lesson-comun-v2/src/app/model.ts
@@ -1,4 +1,4 @@
 export interface Student {
-    id?: number;
+    id?: string;
     name: string;
   }
--- angular/lesson-comun-v2/src/app/srv/student.service.ts
+++ angular/lesson-comun-v2/src/app/srv/student.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Student } from '../model';
 import { BehaviorSubject, Observable, from, of, take } from 'rxjs';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +11,17 @@ export class StudentService {
   constructor() { }
 
   studentsArr: Student[] = [
-    { id: 1, name: 'Ivan' },
-    { id: 2, name: 'Bill' },
-    { id: 3, name: 'John' },
-    { id: 4, name: 'Petr' },
+    { id: uuidv4(), name: 'Ivan' },
+    { id: uuidv4(), name: 'Bill' },
+    { id: uuidv4(), name: 'John' },
+    { id: uuidv4(), name: 'Petr' },
   ]
 
   students: BehaviorSubject<Student[]> = new BehaviorSubject<Student[]>([
-    { id: 1, name: 'Ivan' },
-    { id: 2, name: 'Bill' },
-    { id: 3, name: 'John' },
-    { id: 4, name: 'Petr' },
+    { id: uuidv4(), name: 'Ivan' },
+    { id: uuidv4(), name: 'Bill' },
+    { id: uuidv4(), name: 'John' },
+    { id: uuidv4(), name: 'Petr' },
   ])
 
   //В рамках хорошего тона отдельный Observable
@@ -35,33 +36,33 @@ export class StudentService {
     { id: 3, name: 'John' },
     { id: 4, name: 'Petr' },
   ],
-  [
-    { id: 1, name: 'Ivan1' },
-    { id: 2, name: 'Bill1' },
-    { id: 3, name: 'John1' },
-    { id: 4, name: 'Petr1' },
-  ],
-  [
-    { id: 1, name: 'Ivan2' },
-    { id: 2, name: 'Bill2' },
-    { id: 3, name: 'John2' },
-    { id: 4, name: 'Petr2' },
-  ]
+    [
+      { id: 1, name: 'Ivan1' },
+      { id: 2, name: 'Bill1' },
+      { id: 3, name: 'John1' },
+      { id: 4, name: 'Petr1' },
+    ],
+    [
+      { id: 1, name: 'Ivan2' },
+      { id: 2, name: 'Bill2' },
+      { id: 3, name: 'John2' },
+      { id: 4, name: 'Petr2' },
+    ]
   )
 
   //streamStud1$ = new Observable<Student[]>(this.studentsArr)
 
 
-  ttt(){
-/*     this.students$.pipe(take(3)).subscribe({
-      next: v => {let i = 0; console.log(v[i].name); i++}
-    }) */
+  ttt() {
+    /*     this.students$.pipe(take(3)).subscribe({
+          next: v => {let i = 0; console.log(v[i].name); i++}
+        }) */
 
     let i: number = 0;
-/*     this.stream$.pipe(take(3)).subscribe(val => {
-      console.log(`Value ${i} :`, val)
-      i++;
-    }) */
+    /*     this.stream$.pipe(take(3)).subscribe(val => {
+          console.log(`Value ${i} :`, val)
+          i++;
+        }) */
 
     this.stream2$.pipe(take(3)).subscribe(val => {
       console.log(val)
@@ -71,29 +72,47 @@ export class StudentService {
 
   addName(name: string) {
 
-    let i: number = 0;
+    /* let i: number = 0; */
+
+    const newStudent = {
+      name: name,
+      id: uuidv4()//stdLenght + 1
+    }
 
     // После того как выполнена операция pipe(take(1)) мы можем подписаться по окончании ее выполнения и подписаться чтобы обработать результат
-    this.students$.pipe(take(2)).subscribe((val) => {
+    this.students$/* .pipe(take(1)) */.subscribe((val) => {
 
-      console.log(val[i].name);
-      console.log(i);
-      i++;
+      console.log("Something has changed in the array")
 
-/*       let stdLenght = 0;
-      this.students$.subscribe((students) => {
-        stdLenght = students.length
-      })
+      /*       console.log(val[i].name);
+            console.log(i);
+            i++; */
 
-      const newStudent = {
-        name: name,
-        id: stdLenght + 1
-      }
+      /*       let stdLenght = 0;
+            this.students$.subscribe((students) => {
+              stdLenght = students.length
+            }) */
 
-      const newArray = [...val, newStudent];
-      this.students.next(newArray); */
+      /*       const newArray = [...val, newStudent];
+            this.students.next(newArray); */
+
+      console.log(val)
     })
 
+  }
+
+
+  observable = new Observable(observer => {
+    observer.next('Hello');
+    observer.next('World');
+    observer.complete();
+  });
+
+  runSubscr() {
+    this.observable.subscribe({
+      complete: () => { console.log("completed") },
+      next: (v) => { console.log(v) }
+    });
   }
 
 }
