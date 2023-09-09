d85a627f7dabab69750a0407d91b57589d40f14a
Roman Aleshin <aleshin.roman.nk@gmail.com>
2023-09-09 22:52:24 +0700

    09-09-2023

--- ConsoleAppPrototypes/ConsoleAppPrototypes/Program.cs
+++ ConsoleAppPrototypes/ConsoleAppPrototypes/Program.cs
@@ -2,7 +2,30 @@
 
 using System.Collections.Generic;
 
-go();
+List<DateTime> intervals = new List<DateTime>();
+
+//int now = 0;
+
+//for (int i = 0; i < 10; i++)
+//{
+
+//}
+
+DateTime dt = new DateTime(2023, 9, 9, 0, 0, 0);
+
+intervals.Add(dt);
+intervals.Add(dt = dt.AddDays(1));
+intervals.Add(dt = dt.AddDays(1));
+intervals.Add(dt = dt.AddDays(2));
+intervals.Add(dt = dt.AddDays(2));
+intervals.Add(dt = dt.AddDays(3));
+intervals.Add(dt.AddDays(10));
+
+foreach (var item in intervals)
+{
+    Console.WriteLine($"{item} | {item.DayOfWeek}");
+}
+
 
 Console.ReadLine();
 
